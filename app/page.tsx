1f35289b784f299e58e7b38e283fc875d1c912d5
vector17002 <vector17002>
2024-11-16 23:52:35 +0530

    updated text sizing and new branch added

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "@/components/Home/hero";
+import Projects from "@/components/Home/projects";
 import Skills from "@/components/Home/skills";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="w-full h-full text-xl flex items-center justify-center mx-auto my-5">
       <div className="flex flex-col justify-center items-start w-full max-w-2xl mx-auto mb-16">
       <Hero/>
+      <Projects/>
       <Skills/>
       </div>
     </main>
--- components/Home/skills.tsx
+++ components/Home/skills.tsx
@@ -4,10 +4,10 @@ import { skills } from '@/lib/constants'
 const Skills = () => {
   return (
     <div id='skills' className='w-full flex flex-col mx-2 mt-10'>
-        <p className='text-2xl md:text-4xl font-bold'>Skills</p>
+        <p className='text-2xl md:text-4xl font-extrabold'>Skills</p>
         <div className='flex flex-wrap gap-3 mt-10 text-lg'>
             {skills.map((skill) => (
-                <Badge className='px-2 py-1 rounded-xl'>{skill}</Badge>
+                <Badge className='px-2 py-1 rounded-xl' key={skill}>{skill}</Badge>
             ))}
         </div>
     </div>
